078a912f1495229cc4197210c774ab4e20f6c038
nisticism <nistacular@gmail.com>
2020-06-13 08:39:50 -0700

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +10,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(id: 0)
+    @recipe.ingredients.build(id: 1)
   end
 
   def create
+    recipe = Recipe.create(recipe_params)
+    # //binding.pry
   end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
+  end
+
 end
